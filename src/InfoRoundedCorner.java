5d219973b521127d8fbb9dae4a2188251713cdd2
Maze711 <badlonmazeclarion@gmail.com>
2024-04-12 16:27:30 +0800

    TITLE IMPLEMENTED, BORDER RADIUS ADJUSTED.

--- src/InfoRoundedCorner.java
+++ src/InfoRoundedCorner.java
@@ -2,14 +2,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.Serializable;
 
-
-public class InfoRoundedCorner extends JPanel implements Serializable  {
-	
-	private static final long serialVersionUID = 1L;
-	private int cornerRadius;
+public class InfoRoundedCorner extends JPanel implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int cornerRadius;
+    private Color startColor;
+    private Color endColor;
 
     public InfoRoundedCorner(int cornerRadius) {
         this.cornerRadius = cornerRadius;
+        this.startColor = Color.decode("#00537A");
+        this.endColor = Color.decode("#0C1F34");
+        setOpaque(false);
+    }
+
+    public InfoRoundedCorner(int cornerRadius, Color startColor, Color endColor) {
+        this.cornerRadius = cornerRadius;
+        this.startColor = startColor;
+        this.endColor = endColor;
         setOpaque(false);
     }
 
@@ -20,9 +29,15 @@ public class InfoRoundedCorner extends JPanel implements Serializable  {
         int width = getWidth();
         int height = getHeight();
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setColor(getBackground());
+
+        // Create a gradient paint from top to bottom
+        GradientPaint gradientPaint = new GradientPaint(0, 0, startColor, 0, height, endColor);
+
+        // Set the paint to the graphics context
+        g2d.setPaint(gradientPaint);
+
+        // Fill the rounded rectangle with the gradient paint
         g2d.fillRoundRect(0, 0, width - 1, height - 1, cornerRadius, cornerRadius);
         g2d.dispose();
     }
 }
-
