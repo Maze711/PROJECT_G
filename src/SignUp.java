16d58c4bb2a0a9fe5da76e04a0bf2f5ee2bb88c2
MarkPete0207 <florentinomark18@gmail.com>
2024-04-15 20:04:56 +0800

    Commit and push by Pete

--- src/SignUp.java
+++ src/SignUp.java
@@ -1,14 +1,66 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Color;
 import javax.swing.JPanel;
+import java.awt.SystemColor;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
 
-public class SignUp extends JPanel {
+public class SignUp {
 
-	private static final long serialVersionUID = 1L;
+	private JFrame frame;
 
 	/**
-	 * Create the panel.
+	 * Launch the application.
 	 */
-	public SignUp() {
-
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SignUp window = new SignUp();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
+	/**
+	 * Create the application.
+	 */
+	public SignUp() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(0, 0, 0));
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 10, 1083, 648);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(SystemColor.inactiveCaptionBorder);
+		panel_1.setBounds(569, 0, 514, 648);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(0, 0, 571, 648);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+		frame.setBounds(100, 100, 1117, 705);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+	}
 }
