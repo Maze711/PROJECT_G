1aebb696904e616e40c0512e003ad058e273f793
Maze711 <badlonmazeclarion@gmail.com>
2024-04-25 13:08:08 +0800

    CANCEL BUTTON FUNCTION

--- src/CreateNew.java
+++ src/CreateNew.java
@@ -1,6 +1,9 @@
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -8,6 +11,7 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
 public class CreateNew {
 
@@ -29,11 +33,12 @@ public class CreateNew {
 			}
 		});
 	}
-	
-	public void CreateNew(){
+
+	public void CreateNew() {
 		JFrame create = frame;
 		create.show();
 	}
+
 	/**
 	 * Create the application.
 	 */
@@ -53,45 +58,52 @@ public class CreateNew {
 		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.getContentPane().setLayout(null);
-		
-		//Fonts
+
+		// Fonts
 		Font PrimaryFont = FontLoader.getFont("Primary", 30);
 		Font SecondaryFont = FontLoader.getFont("Secondary", 15);
 		Font PrimaryEBFont = FontLoader.getFont("PrimaryEB32", 24);
 		Font SemiB = FontLoader.getFont("SemiB", 24);
-		Font ExtraBold = FontLoader.getFont("ExtraBold",24);
-		Font ExtraBold2 = FontLoader.getFont("ExtraBold",12);
-		
+		Font ExtraBold = FontLoader.getFont("ExtraBold", 24);
+		Font ExtraBold2 = FontLoader.getFont("ExtraBold", 12);
+
 		JPanel panel = new JPanel();
 		panel.setBounds(0, 0, 454, 250);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
-		
+
 		JLabel txtCreate = new JLabel("Create New Record");
 		txtCreate.setBackground(new Color(16, 10, 14));
 		txtCreate.setBounds(46, 55, 362, 44);
 		txtCreate.setFont(ExtraBold);
 		panel.add(txtCreate);
-		
+
 		txtInputYear = new RoundTxtField(40, new Color(0x0B1E33), 3);
 		txtInputYear.setHorizontalAlignment(SwingConstants.LEFT);
 		txtInputYear.setText("     Input Year");
 		txtInputYear.setBounds(46, 110, 362, 46);
 		panel.add(txtInputYear);
 		txtInputYear.setColumns(10);
-		
-		RoundButton btnCancel = new RoundButton("Cancel",8,Color.decode("#FFBA42"));
+
+		RoundButton btnCancel = new RoundButton("Cancel", 8, Color.decode("#FFBA42"));
 		btnCancel.setBounds(174, 175, 112, 40);
 		btnCancel.setFont(ExtraBold2);
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFrame frame = (JFrame) SwingUtilities.getWindowAncestor((Component) e.getSource());
+				frame.dispose();
+			}
+		});
 		panel.add(btnCancel);
-		
-		RoundButton btnCreate = new RoundButton("Create",8,Color.decode("#0B1E33"));
+
+		RoundButton btnCreate = new RoundButton("Create", 8, Color.decode("#0B1E33"));
 		btnCreate.setForeground(new Color(255, 255, 255));
 		btnCreate.setBackground(new Color(255, 255, 255));
 		btnCreate.setBounds(296, 175, 112, 40);
 		btnCreate.setFont(ExtraBold2);
 		panel.add(btnCreate);
 		frame.setLocationRelativeTo(null);
-		frame.setUndecorated(true); 
+		frame.setUndecorated(true);
 	}
+
 }
